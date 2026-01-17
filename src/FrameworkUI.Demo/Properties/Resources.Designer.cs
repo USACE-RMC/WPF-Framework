a4fd799e0c592ff1181ecc48eb202d9dbd173d34
HadenSmith <cole.h.smith@usace.army.mil>
2026-01-17 14:01:25 -0700

    Trying to clean icons

--- src/FrameworkUI.Demo/Properties/Resources.Designer.cs
+++ src/FrameworkUI.Demo/Properties/Resources.Designer.cs
@@ -90,26 +90,7 @@ namespace FrameworkUI.Demo.Properties {
             }
         }
         
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Add {
-            get {
-                object obj = ResourceManager.GetObject("Add", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Delete {
-            get {
-                object obj = ResourceManager.GetObject("Delete", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
+             
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
