cd914e14551b8020c10ae98cbe6056a2380b0139
HadenSmith <cole.h.smith@usace.army.mil>
2026-01-17 13:50:48 -0700

    Cleaning up icons

--- src/FrameworkUI/Properties/Resources.Designer.cs
+++ src/FrameworkUI/Properties/Resources.Designer.cs
@@ -117,24 +117,5 @@ namespace FrameworkUI.Properties {
             }
         }
 
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Group {
-            get {
-                object obj = ResourceManager.GetObject("Group", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Ungroup {
-            get {
-                object obj = ResourceManager.GetObject("Ungroup", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
--- src/NumericControls/Properties/Resources.Designer.cs
+++ src/NumericControls/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace NumericControls.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        public static System.Drawing.Bitmap EditButtonIcon {
-            get {
-                object obj = ResourceManager.GetObject("EditButtonIcon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
